1e032a7dc61292a94415721742311ca04649fb0a
Michal Kulisiewicz <michalkulisiewicz@gmail.com>
2020-12-23 03:17:41 +0100

    Added streamlit interface, created user inputs for a scraper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ali_scrapy.ali.run_scraper import Scraper
 from pathlib import Path
-import sys, os
+import sys
+import streamlit as st
 
 class Main():
     def __init__(self):
@@ -11,7 +12,24 @@ class Main():
         goal_dir = cwd / 'ali_scrapy'
         sys.path.append(str(goal_dir))
 
+    def run(self):
+        st.title('Dropshipper')
+        st.header('Aliexpress_Scraper: ')
+
+        # Text Input
+        ali_cat = st.text_input("Podaj id kategorii aliexpres: ", "Pisz Tutaj...")
+        allegro_cat = st.text_input("Podaj id kategorii Allegro odpowiadającej tej z aliexpress: ", "Pisz Tutaj...")
+        scrape_qty = st.text_input("Podaj ilość przedmiotów, które mają zostać dodane do sklepu: ", "Pisz Tutaj...")
+        if st.button('Dodaj do sklepu'):
+            scraper = Scraper()
+            scraper.run_cat_spider('200003482', 1)
+
+
+        #     result = name.title()
+        #     st.success(result)
+        # else:
+        #     st.write("Press the above button..")
+
 if __name__ == '__main__':
     main = Main()
-    scraper = Scraper()
-    scraper.run_cat_spider('200003482', 1)
+    main.run()
