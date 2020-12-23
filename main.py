ccbbf7fb952cdc5a2ee98f700ad995e4bc6f1ba6
Michal Kulisiewicz <michalkulisiewicz@gmail.com>
2020-12-23 02:51:36 +0100

    Fixed issue with path when running from terminal

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from ali_scrapy.ali.run_scraper import Scraper
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pathlib import Path
+import sys, os
 
+class Main():
+    def __init__(self):
+        self.add_cwd_to_path()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def add_cwd_to_path(self):
+        cwd = Path.cwd()
+        goal_dir = cwd / 'ali_scrapy'
+        sys.path.append(str(goal_dir))
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    main = Main()
     scraper = Scraper()
     scraper.run_cat_spider('200003482', 1)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
